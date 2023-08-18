982fbe15fdf0349283870240f9c562a64812fed7
mypc <drinkinswo@gmail.com>
2023-08-18 11:11:12 +0300

    init

---
+++ src/test/java/com/job4j/accidents/Job4jAccidentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.job4j.accidents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Job4jAccidentsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
