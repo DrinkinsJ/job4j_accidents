0b22b319a657c68edb5f3cf0e90a6e251a1a5f90
mypc <drinkinswo@gmail.com>
2023-08-27 17:42:41 +0300

    Авторизация через JDBC

---
+++ db/scripts/006_dml_create_users_table.sql
@@ -0,0 +1,7 @@
+create TABLE users
+(
+    username VARCHAR(50)  NOT NULL,
+    password VARCHAR(100) NOT NULL,
+    enabled  boolean default true,
+    PRIMARY KEY (username)
+);
--- src/main/java/com/job4j/accidents/controller/IndexController.java
+++ src/main/java/com/job4j/accidents/controller/IndexController.java
@@ -3,6 +3,7 @@ package com.job4j.accidents.controller;
 import com.job4j.accidents.repository.HbmAccidentRepository;
 import com.job4j.accidents.service.SpringAccidentService;
 import lombok.AllArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,7 +16,7 @@ public class IndexController {
 
     @GetMapping({"/", "/index"})
     public String getIndex(Model model) {
-        model.addAttribute("user", "Petr Arsentev");
+        model.addAttribute("user", SecurityContextHolder.getContext().getAuthentication().getPrincipal());
         model.addAttribute("accidents", accidentService.findAll());
         return "index";
     }
