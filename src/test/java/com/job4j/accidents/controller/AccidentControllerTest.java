8f6c42b75e7dffaec0a6dc1b0ec420839d18cd0b
mypc <drinkinswo@gmail.com>
2023-08-30 19:03:20 +0300

    Spring test Controllers GET

---
+++ src/test/java/com/job4j/accidents/controller/AccidentControllerTest.java
@@ -0,0 +1,43 @@
+package com.job4j.accidents.controller;
+
+import com.job4j.accidents.Job4jAccidentsApplication;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@SpringBootTest(classes = Job4jAccidentsApplication.class)
+@AutoConfigureMockMvc
+public class AccidentControllerTest {
+
+    @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    @WithMockUser
+    public void shouldReturnDefaultMessageCreatePage() throws Exception {
+        this.mockMvc.perform(get("/createAccident"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("createAccident"));
+    }
+
+    @Test
+    @WithMockUser
+    public void shouldReturnDefaultMessageEditPage() throws Exception {
+        this.mockMvc.perform(get("/editAccident/1"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("editAccident"));
+    }
+
+}
